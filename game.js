dfc2d32ff6993038d9aa60a574993f53d0811462
André Felipe <111099027+dedsfe@users.noreply.github.com>
2024-02-21 19:55:37 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,25 +1,92 @@
 
-var audioGreen = $('greenSound');
-var audioBlue = $('blueSound');
-var audioYellow = $('yellowSound');
-var audioRed = $('redSound');
+var audioGreen = $('#greenSound');
+var audioBlue = $('#blueSound');
+var audioYellow = $('#yellowSound');
+var audioRed = $('#redSound');
+var level = 0;
 
-
-
-
-gamePattern = []
+gamePattern = [];
+userClickedPattern = [];
 buttonColours = ["red","blue","green","yellow"];
 
-function nextSequence() {
-    var randomNumber = Math.round(Math.random() * buttonColours.length);
+
+//Começa o jogo
+$(document).on("keypress", function(){
+    if (level < 1){
+        nextSequence();
+        $("#level-title").text("level = "+level);
+    }else{
+        
+     }
+});
+
+function starGame(){
+    isSecureContext.self.Math(starGame)
+};
+
+
+//Seleciona uma cor aleatória e coloca dentro do gamePattern
+function nextSequence()
+{
+    var randomNumber = Math.round(Math.random() * 3);
     randomChosenColour = buttonColours[randomNumber]
     gamePattern.push(randomChosenColour)
-    return gamePattern[0]
-}
+    gamePattern[0]
+    level ++;
 
-function flashAudio(){
-    $('#' + nextSequence()).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    audio.play($('#'+nextSequence()+'Sound'));
-}
+};
+
+
+//Faz piscar
+function flash()
+{
+    $('#' + gamePattern).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+};
+//Faz piscar o elemento clicado
+function flashClicked(id)
+{
+    $('#' + id).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+};
+
+
+//Toca o audio
+function playColorSound(color) 
+{
+    var audio = document.getElementById(color + "Sound");
+    audio.play();
+    return 
+};
+
+//faz a cor mudar do elemento clicado
+function animatePress(currentColour){
+    $('#' + currentColour).addClass("pressed")
+    var self = $('#' + currentColour);
+    
+    setTimeout(function(){
+        self.removeClass("pressed");
+    }, 100);
+};
+
+  
+//Ao clicar no botão ele pisca e faz som.
+$("#"+gamePattern[0]).on("click",function()
+{
+    playColorSound(nextSequence());
+});
+
+
+$('.btn').on('click', function(){
+    let color = $(this).attr("id");
+    playColorSound(color);  
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+    flashClicked(color);
+    animatePress(color);
+});
+
+
 
 
